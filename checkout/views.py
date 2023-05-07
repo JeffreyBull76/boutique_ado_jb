cce09c4218a4e8256b07e00d84809c2b77dc72ea
JeffreyBull76 <jeffreybull@sky.com>
2023-05-07 16:11:07 +0000

    add basic stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N59fdIHUuv4NymJanD3n8dpT9HBMCFXX6NW0v3TmO2FzIlPjGZdfuqZtsss9z9fl7cDFFGBzlVPRdCdWyM3FWRi00Mp341qHC',  # noqa
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
